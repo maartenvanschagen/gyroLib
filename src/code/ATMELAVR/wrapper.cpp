ef22d468cdce7c9c0743ef0d91e712fd1743e69a
maartennl2002 <maarten@marcovanschagen.nl>
2019-08-14 21:34:56 -0400

    finished delay wrapper function for avr

--- src/code/ATMELAVR/wrapper.cpp
+++ src/code/ATMELAVR/wrapper.cpp
@@ -1,19 +1,20 @@
 #ifdef __AVR__
   #include "wrapper.h"
   #include <stdint.h>
+  #include <util/delay.h>
 
   namespace wrapper{
     void delayMillis(const uint64_t milliseconds){
-      
+      _delay_ms(milliseconds); 
     }
     uint64_t getMicros(){
       return 0;
     }
-}
+  }
 
-extern "C" void __cxa_pure_virtual()
-{
-  //TODO: handle undefined pure function here
-}
+  extern "C" void __cxa_pure_virtual()
+  {
+    //TODO: handle undefined pure function here
+  }
 
 #endif
